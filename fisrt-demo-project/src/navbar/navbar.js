2568350c1569076903d9ea2874da775dcbea473c
hishamrehab <hishamrehab30@gmail.com>
2026-01-25 20:23:49 +0200

    component life cycle methods

--- fisrt-demo-project/src/navbar/navbar.js
+++ fisrt-demo-project/src/navbar/navbar.js
@@ -5,26 +5,32 @@ export default class Navbar extends React.Component {
     constructor() {
         super();
         console.log("constructor");
+
+        this.state ={
+          name :"hisham",
+          title: "Devleopre"
+        };
     }
 
     componentDidMount() {
       console.log("from componentDidMount");
+      this.setState({name : "Ahmed"});
     }
 
     componentDidUpdate() {
       console.log("from componentDidUpdate");
     }
 
-    componentWillUnmount() { //clean up method
-      console.log("from componentWillUnmount");
-    }
+    // componentWillUnmount() { //clean up method
+    //   console.log("from componentWillUnmount");
+    // }
 
 
   render() {
     console.log("render");
     return(
       <>
-        <h1>this is Navbar</h1>
+        <h1>My Name is {this.state.name}</h1>
         <p>Hello</p>
       </>
     )
