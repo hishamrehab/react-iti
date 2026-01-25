cda9c9ad8d15f3531271b4efed6758f063a72973
hishamrehab <hishamrehab30@gmail.com>
2026-01-25 20:32:02 +0200

    class component life cycle methods

--- fisrt-demo-project/src/navbar/navbar.js
+++ fisrt-demo-project/src/navbar/navbar.js
@@ -1,38 +1,63 @@
-import React from "react"
+import React from "react";
 
+export default class Navbar
+  extends React.Component
+{
+  constructor() {
+    super();
+    console.log(
+      "constructor",
+    );
 
-export default class Navbar extends React.Component {
-    constructor() {
-        super();
-        console.log("constructor");
+    this.state =
+      {
+        name: "hisham",
+        title:
+          "Devleopre",
+      };
+  }
 
-        this.state ={
-          name :"hisham",
-          title: "Devleopre"
-        };
-    }
+  componentDidMount() {
+    console.log(
+      "from componentDidMount",
+    );
+    this.setState(
+      {
+        name: "Ahmed",
+      },
+    );
+  }
 
-    componentDidMount() {
-      console.log("from componentDidMount");
-      this.setState({name : "Ahmed"});
-    }
+  componentDidUpdate() {
+    console.log(
+      "from componentDidUpdate",
+    );
+  }
 
-    componentDidUpdate() {
-      console.log("from componentDidUpdate");
-    }
-
-    // componentWillUnmount() { //clean up method
-    //   console.log("from componentWillUnmount");
-    // }
-
+  // componentWillUnmount() { //clean up method
+  //   console.log("from componentWillUnmount");
+  // }
 
   render() {
-    console.log("render");
-    return(
+    console.log(
+      "render",
+    );
+    return (
       <>
-        <h1>My Name is {this.state.name}</h1>
-        <p>Hello</p>
+        <h1>
+          My
+          Name
+          is{" "}
+          {
+            this
+              .state
+              .name
+          }
+        </h1>
+        <p>
+          Hello
+        </p>
       </>
-    )
+    );
   }
 }
